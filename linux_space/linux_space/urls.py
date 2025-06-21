afe817bec4412385434fdef3f9e85e68754f9a85
valjean-h8 <valjean.h8@gmail.com>
2025-06-21 12:14:21 +0300

    Add techlog.urls

--- linux_space/linux_space/urls.py
+++ linux_space/linux_space/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
 from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
-    path("techlog/", include("techlog.urls")),
+    path("", include("techlog.urls")),
     path('admin/', admin.site.urls),
 ]
