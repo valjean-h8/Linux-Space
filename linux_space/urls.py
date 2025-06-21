cb67320fe2877b575a38013bb47bcb61897a9031
valjean-h8 <valjean.h8@gmail.com>
2025-06-21 14:12:01 +0300

    Restructured project

--- linux_space/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from django.contrib import admin
-
-urlpatterns = [
-    path("", include("techlog.urls")),
-    path('admin/', admin.site.urls),
-]
--- techlog/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TechlogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'techlog'
